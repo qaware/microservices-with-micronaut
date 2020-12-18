14814b91676819b1f41ee22763bb642afa43268a
Moritz Kammerer <moritz.kammerer@qaware.de>
2020-12-18 10:00:00 +0100

    Update to newest Micronaut and Gradle

--- github-scraper/src/main/java/github/scraper/Application.java
+++ github-scraper/src/main/java/github/scraper/Application.java
@@ -3,7 +3,6 @@ package github.scraper;
 import io.micronaut.runtime.Micronaut;
 
 public class Application {
-
     public static void main(String[] args) {
         Micronaut.run(Application.class);
     }
